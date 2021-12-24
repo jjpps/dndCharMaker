4a86d475b9549d203e992424fc4b8ab2ffea32a8
jpps <joaopedro-ps@hotmail.com>
2021-12-24 00:46:25 -0300

    login mecanic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  
-  
+export class AppComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
+
+  logged: boolean = false;
+  checkoutForm = this.formBuilder.group({
+    user: '',
+    password: '',
+  });
+  ngOnInit(): void {
+    console.log(this.logged);
+  }
+  isLoggedIn() {
+    return this.logged;
+  }
+
+  onSubmit(event: Event): void {
+    // Process checkout data here
+    event.preventDefault();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.logged = true;
+    this.checkoutForm.reset();
+  }
 }
