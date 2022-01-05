f9ce40b969310c7542f659516ab01400f72a2c05
JP <j.pedro@conductor.com.br>
2022-01-05 16:15:24 -0300

    menu sheet race

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SheetComponent } from './sheet/sheet.component';
-
+import { CreateSheetComponent } from './create-sheet/create-sheet.component';
 
 const routes: Routes = [
   { path: 'home', component: AppComponent },
   { path: 'sheet', component: SheetComponent },
+  { path: 'CreateSheet', component: CreateSheetComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
