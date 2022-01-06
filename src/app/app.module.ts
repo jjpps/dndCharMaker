936c6d8d8758fa50c8a06a02a4348c7ae6cac3d4
JP <j.pedro@conductor.com.br>
2022-01-06 15:03:17 -0300

    draft steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,4 +16,4 @@ import { ChoseRaceComponent } from './create-sheet/chose-race/chose-race.compone
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/create-sheet/create-sheet.component.ts
+++ src/app/create-sheet/create-sheet.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-create-sheet',
   templateUrl: './create-sheet.component.html',
   styleUrls: ['./create-sheet.component.css']
 })
 export class CreateSheetComponent implements OnInit {
-
-  constructor() { }
+  personalDetails!: FormGroup;
+  addressDetails!: FormGroup;
+  educationalDetails!: FormGroup;
+  personal_step = false;
+  address_step = false;
+  education_step = false;
+  step = 1;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.personalDetails = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required]
+    });
+    this.addressDetails = this.formBuilder.group({
+      city: ['', Validators.required],
+      address: ['', Validators.required],
+      pincode: ['', Validators.required]
+    });
+    this.educationalDetails = this.formBuilder.group({
+      highest_qualification: ['', Validators.required],
+      university: ['', Validators.required],
+      total_marks: ['', Validators.required]
+    });
+  }
+  get personal() { return this.personalDetails.controls; }
+  get education() { return this.educationalDetails.controls; }
+  get address() { return this.addressDetails.controls; }
+
+  next() {
+    if (this.step == 1) {
+      this.personal_step = true;
+      if (this.personalDetails.invalid) { return }
+      this.step++
+    }
+    if (this.step == 2) {
+      this.address_step = true;
+      if (this.addressDetails.invalid) { return }
+      this.step++;
+    }
+  }
+  previous() {
+    this.step--
+    if (this.step == 1) {
+      this.personal_step = false;
+    }
+    if (this.step == 2) {
+      this.education_step = false;
+    }
   }
 
 }
